9fb65d23c7932b58ca12d8bac4228c28f15446e0
Fahad Heylaal <fahad19@gmail.com>
2023-03-05 18:36:17 +0100

    CLI package

---
+++ packages/cli/README.md
@@ -0,0 +1,66 @@
+# @featurevisor/cli
+
+CLI package of Featurevisor.
+
+Visit [https://featurevisor.com](https://featurevisor.com) for more information.
+
+## Installation
+
+```
+$ npm install -g @featurevisor/cli
+```
+
+Or via `npx`:
+
+```
+$ npx featurevisor <command>
+```
+
+## Usage
+
+### `init`
+
+Initialize your project.
+
+```
+$ featurevisor init
+```
+
+If you want to initialize your project with a specific example:
+
+```
+$ featurevisor init --example <name>
+```
+
+Examples found [here](../../examples).
+
+### `lint`
+
+Check if your YAMLs are valid.
+
+```
+$ featurevisor lint
+```
+
+
+### `build`
+
+Build your datafiles.
+
+```
+$ featurevisor build
+```
+
+### `test`
+
+Test your generated datafiles using the SDK, against specs written in YAMLs.
+
+```
+$ featurevisor test
+```
+
+See test specs in YAMLs [here](../../examples/example-1/tests) for inspiration.
+
+## License
+
+MIT © [Fahad Heylaal](https://fahad19.com)
---
+++ packages/cli/src/index.ts
@@ -0,0 +1,116 @@
+import * as fs from "fs";
+import * as path from "path";
+
+import * as yargs from "yargs";
+
+import {
+  CONFIG_MODULE_NAME,
+  getProjectConfig,
+  lintProject,
+  testProject,
+  buildProject,
+  initProject,
+} from "@featurevisor/core";
+
+process.on("unhandledRejection", (reason, p) => {
+  console.error(reason);
+  process.exit(1);
+});
+
+function requireConfigFile(configModulePath) {
+  if (!fs.existsSync(configModulePath)) {
+    console.error("No config file found. Please create `featurevisor.config.js` file first.");
+
+    process.exit(1);
+  }
+}
+
+function requireAndGetProjectConfig(rootDirectoryPath) {
+  const configModulePath = path.join(rootDirectoryPath, CONFIG_MODULE_NAME);
+
+  requireConfigFile(configModulePath);
+
+  return getProjectConfig(rootDirectoryPath);
+}
+
+async function main() {
+  const rootDirectoryPath = process.cwd();
+
+  yargs(process.argv.slice(2))
+    .usage("Usage: <command> [options]")
+
+    /**
+     * Commands
+     */
+    .command({
+      command: "init",
+      handler: async function (options) {
+        const hasError = await initProject(rootDirectoryPath, options.example);
+
+        if (hasError) {
+          process.exit(1);
+        }
+      },
+    })
+    .example("$0 init", "scaffold a new project")
+    .example("$0 init --example=exampleName", "scaffold a new project from known example")
+
+    .command({
+      command: "lint",
+      handler: async function (options) {
+        const projectConfig = requireAndGetProjectConfig(rootDirectoryPath);
+
+        const hasError = await lintProject(projectConfig);
+
+        if (hasError) {
+          process.exit(1);
+        }
+      },
+    })
+    .example("$0 lint", "lint all YAML file content")
+
+    .command({
+      command: "build",
+      handler: function (options) {
+        const projectConfig = requireAndGetProjectConfig(rootDirectoryPath);
+
+        buildProject(rootDirectoryPath, projectConfig);
+      },
+    })
+    .example("$0 build", "build datafiles")
+
+    .command({
+      command: "test",
+      handler: function (options) {
+        const projectConfig = requireAndGetProjectConfig(rootDirectoryPath);
+
+        const hasError = testProject(rootDirectoryPath, projectConfig);
+
+        if (hasError) {
+          process.exit(1);
+        }
+      },
+    })
+    .example("$0 test", "test features")
+
+    /**
+     * Options
+     */
+
+    // @TODO: add --config option
+
+    /**
+     * Help
+     */
+
+    .command({
+      command: "*",
+      handler() {
+        yargs.showHelp();
+      },
+    }).argv;
+
+  return;
+}
+
+main();
