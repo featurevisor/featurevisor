d7aa0e27efdcb041642e77d64a7184f1ed589c5f
Fahad Heylaal <fahad19@gmail.com>
2023-03-26 21:18:26 +0200

    setup webpack for react

---
+++ packages/site/src/components/App.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export function App() {
+  return (
+    <div>
+      <h1>Featurevisor site</h1>
+    </div>
+  )
+}
--- packages/site/webpack.config.js
+++ packages/site/webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 const getWebpackConfig = require("../../tools/getWebpackConfig");
 
 const wepbackConfig = getWebpackConfig({
-  entryFilePath: path.join(__dirname, "src", "index.ts"),
+  entryFilePath: path.join(__dirname, "src", "index.tsx"),
   entryKey: "index",
   outputDirectoryPath: path.join(__dirname, "dist"),
   outputLibrary: "FeaturevisorSite",
