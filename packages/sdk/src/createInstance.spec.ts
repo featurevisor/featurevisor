f1ddb0cc7056778b2bd9f1a792141deb3979364b
Fahad Heylaal <fahad19@gmail.com>
2023-04-17 20:38:00 +0200

    tests

--- packages/sdk/src/createInstance.spec.ts
+++ packages/sdk/src/createInstance.spec.ts
@@ -1,3 +1,5 @@
+import { DatafileContent } from "@featurevisor/types";
+
 import { createInstance } from "./createInstance";
 
 describe("sdk: createInstance", function () {
@@ -66,5 +68,71 @@ describe("sdk: createInstance", function () {
 
     expect(variation).toEqual(true);
     expect(intercepted).toEqual(true);
+  });
+
+  it("should refresh datafile", function (done) {
+    let revision = 1;
+    let refreshed = false;
+    let updated = false;
+
+    function getDatafileContent(): DatafileContent {
+      const content: DatafileContent = {
+        schemaVersion: "1",
+        revision: revision.toString(),
+        features: [
+          {
+            key: "test",
+            defaultVariation: false,
+            bucketBy: "userId",
+            variations: [
+              { type: "boolean", value: true },
+              { type: "boolean", value: false },
+            ],
+            traffic: [
+              {
+                key: "1",
+                segments: "*",
+                percentage: 100000,
+                allocation: [
+                  { variation: true, percentage: 100000 },
+                  { variation: false, percentage: 0 },
+                ],
+              },
+            ],
+          },
+        ],
+        attributes: [],
+        segments: [],
+      };
+
+      revision += 1;
+
+      return content;
+    }
+
+    const sdk = createInstance({
+      datafileUrl: "http://localhost:3000/datafile.json",
+      handleDatafileFetch: function (datafileUrl) {
+        return new Promise(function (resolve, reject) {
+          resolve(getDatafileContent());
+        });
+      },
+      refreshInterval: 0.1,
+      onRefresh() {
+        refreshed = true;
+      },
+      onUpdate() {
+        updated = true;
+      },
+    });
+
+    setTimeout(function () {
+      expect(refreshed).toEqual(true);
+      expect(updated).toEqual(true);
+
+      sdk.stopRefreshing();
+
+      done();
+    }, 200);
   });
 });
