8f7509cd5fa5b24e44fe64ce16c420a6a7190a31
Fahad Heylaal <fahad19@gmail.com>
2023-04-29 21:55:38 +0200

    allocator

---
+++ packages/core/src/allocator.ts
@@ -0,0 +1,21 @@
+import { Rule, ExistingFeature, Traffic, Variation, Range, Percentage } from "@featurevisor/types";
+import { MAX_BUCKETED_NUMBER } from "@featurevisor/sdk";
+
+export function getAllocation(availableRanges: Range[], fill: Percentage) {
+  const result: Range[] = [];
+
+  let remaining = fill;
+  let i = 0;
+  while (remaining > 0 && i < availableRanges.length) {
+    const range = availableRanges[i];
+    const rangeFill = Math.min(remaining, range.end - range.start);
+    result.push({
+      start: range.start,
+      end: range.start + rangeFill,
+    });
+    remaining -= rangeFill;
+    i++;
+  }
+
+  return result;
+}
