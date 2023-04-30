9b41a782c558e5196fe0dcb643129c414e2a81f6
Fahad Heylaal <fahad19@gmail.com>
2023-04-30 22:36:39 +0200

    tests

--- packages/core/src/traffic.ts
+++ packages/core/src/traffic.ts
@@ -42,6 +42,10 @@ export function detectIfRangesChanged(
     return false;
   }
 
+  if (!existingFeature.ranges) {
+    return false;
+  }
+
   return JSON.stringify(existingFeature.ranges) !== JSON.stringify(availableRanges);
 }
 
@@ -94,51 +98,56 @@ export function getTraffic(
       rulePercentageDiff <= 0 || // percentage decreased
       rangesChanged; // belongs to a group, and group ranges changed
 
+    let updatedAvailableRanges = JSON.parse(JSON.stringify(availableRanges));
+
+    let lastEnd = 0;
     if (existingTrafficRule && !needsRebucketing) {
       // increase: build on top of existing allocations
+      let existingSum = 0;
+
       traffic.allocation = existingTrafficRule.allocation.map(function ({
         variation,
         percentage, // @TODO: remove it in next breaking semver
         range,
       }) {
-        return {
+        const result = {
           variation,
           percentage, // @TODO remove it in next breaking semver
           range: range || {
-            start: 0,
+            start: lastEnd,
             end: percentage,
           },
         };
-      });
-    }
 
-    let updatedAvailableRanges = JSON.parse(JSON.stringify(availableRanges));
+        existingSum += percentage || 0;
+        lastEnd = lastEnd + (percentage || 0);
+
+        return result;
+      });
+
+      updatedAvailableRanges = getUpdatedAvailableRangesAfterFilling(availableRanges, existingSum);
+    }
 
     variations.forEach(function (variation) {
       const weight = variation.weight as number;
       const percentage = weight * (MAX_BUCKETED_NUMBER / 100);
 
-      let rangesToFill: Range[] = [];
-      if (needsRebucketing) {
-        rangesToFill = getAllocation(updatedAvailableRanges, percentage * (rulePercentage / 100));
-      } else {
-        rangesToFill = getAllocation(
-          updatedAvailableRanges,
-          rulePercentageDiff * (rulePercentage / 100),
-        );
-      }
+      let toFillValue = needsRebucketing
+        ? percentage * (rulePercentage / 100) // whole value
+        : (weight / 100) * rulePercentageDiff; // incrementing
+      const rangesToFill = getAllocation(updatedAvailableRanges, toFillValue);
 
       rangesToFill.forEach(function (range) {
         traffic.allocation.push({
           variation: variation.value,
-          percentage: percentage * (rulePercentage / 100), // @TODO remove it in next breaking semver
+          percentage: toFillValue, // @TODO remove it in next breaking semver
           range,
         });
       });
 
       updatedAvailableRanges = getUpdatedAvailableRangesAfterFilling(
         updatedAvailableRanges,
-        percentage * (rulePercentage / 100),
+        toFillValue,
       );
     });
 
