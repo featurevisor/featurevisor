86ea4b52c93f39b778baf488d6936923d6bbd28e
Fahad Heylaal <fahad19@gmail.com>
2023-06-08 23:35:07 +0200

    chore: tests for bucketing value

--- packages/sdk/src/bucket.spec.ts
+++ packages/sdk/src/bucket.spec.ts
@@ -15,4 +15,23 @@ describe("sdk: Bucket", function () {
       expect(n <= MAX_BUCKETED_NUMBER).toEqual(true);
     });
   });
+
+  // these assertions will be copied to unit tests of SDKs ported to other languages,
+  // so we can keep consitent bucketing across all SDKs
+  it("should return expected number for known keys", function () {
+    const expectedResults = {
+      foo: 20602,
+      bar: 89144,
+      "123.foo": 3151,
+      "123.bar": 9710,
+      "123.456.foo": 14432,
+      "123.456.bar": 1982,
+    };
+
+    Object.keys(expectedResults).forEach((key) => {
+      const n = getBucketedNumber(key);
+
+      expect(n).toEqual(expectedResults[key]);
+    });
+  });
 });
