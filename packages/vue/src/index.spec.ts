d519907a5e76bff8039c96edfd0f04e9154c2e58
Fahad Heylaal <fahad19@gmail.com>
2023-07-06 20:20:59 +0200

    failing vue tests setup

--- packages/vue/src/index.spec.ts
+++
@@ -1,5 +0,0 @@
-describe("vue: index", function () {
-  it("true", function () {
-    expect(true).toEqual(true);
-  });
-});
--- packages/vue/src/useSdk.ts
+++ packages/vue/src/useSdk.ts
@@ -1,8 +1,10 @@
 import { inject } from "vue";
 import { FeaturevisorInstance } from "@featurevisor/sdk";
 
+import { PROVIDER_NAME } from "./setupApp";
+
 export function useSdk(): FeaturevisorInstance {
-  const sdk = inject("featurevisor") as FeaturevisorInstance;
+  const sdk = inject(PROVIDER_NAME) as FeaturevisorInstance;
 
   return sdk;
 }
