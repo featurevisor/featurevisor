3978aaf844e49dea6778c97822638e67763b7b4e
Fahad Heylaal <fahad19@gmail.com>
2023-07-13 22:40:49 +0200

    restore command

---
+++ packages/core/src/restore.ts
@@ -0,0 +1,21 @@
+import * as path from "path";
+import { execSync } from "child_process";
+
+import { ProjectConfig } from "./config";
+
+export function restoreProject(rootDirectoryPath, projectConfig: ProjectConfig) {
+  const relativeStateDirPath = path.relative(rootDirectoryPath, projectConfig.stateDirectoryPath);
+  const cmd = `git checkout -- ${relativeStateDirPath}${path.sep}`;
+
+  try {
+    const output = execSync(cmd, {
+      cwd: rootDirectoryPath,
+    });
+
+    console.log("State files restored successfully.");
+  } catch (e) {
+    console.log("error:", e.message);
+
+    throw new Error("Failed to restore state files.");
+  }
+}
