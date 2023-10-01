e3fc97a2f91553618d223f74dcda71b345f69666
Fahad Heylaal <fahad19@gmail.com>
2023-10-01 21:11:55 +0200

    joi schema updated for linting test specs

--- packages/core/src/linter.ts
+++ packages/core/src/linter.ts
@@ -376,60 +376,39 @@ export function getTestsJoiSchema(
   availableFeatureKeys: string[],
   availableSegmentKeys: string[],
 ) {
-  const testsJoiSchema = Joi.object({
-    tests: Joi.array().items(
+  const segmentTestJoiSchema = Joi.object({
+    segment: Joi.string()
+      .valid(...availableSegmentKeys)
+      .required(),
+    assertions: Joi.array().items(
       Joi.object({
         description: Joi.string().optional(),
-        tag: Joi.string()
-          .valid(...projectConfig.tags)
-          .optional(),
-        environment: Joi.string()
-          .valid(...projectConfig.environments)
-          .optional(),
-        features: Joi.array()
-          .items(
-            Joi.object({
-              key: Joi.string()
-                .valid(...availableFeatureKeys)
-                .required(),
-              assertions: Joi.array().items(
-                Joi.object({
-                  description: Joi.string().optional(),
-                  at: Joi.number().precision(3).min(0).max(100),
-                  context: Joi.object(),
-
-                  // @TODO: one or all below
-                  expectedToBeEnabled: Joi.boolean().required(),
-                  expectedVariation: Joi.alternatives().try(
-                    Joi.string(),
-                    Joi.number(),
-                    Joi.boolean(),
-                  ),
-                  expectedVariables: Joi.object(),
-                }),
-              ),
-            }),
-          )
-          .optional(),
-        segments: Joi.array().items(
-          Joi.object({
-            key: Joi.string()
-              .valid(...availableSegmentKeys)
-              .required(),
-            assertions: Joi.array().items(
-              Joi.object({
-                description: Joi.string().optional(),
-                context: Joi.object(),
-                expectedToMatch: Joi.boolean(),
-              }),
-            ),
-          }),
-        ),
+        context: Joi.object(),
+        expectedToMatch: Joi.boolean(),
       }),
     ),
   });
 
-  return testsJoiSchema;
+  const featureTestJoiSchema = Joi.object({
+    feature: Joi.string()
+      .valid(...availableFeatureKeys)
+      .required(),
+    assertions: Joi.array().items(
+      Joi.object({
+        description: Joi.string().optional(),
+        at: Joi.number().precision(3).min(0).max(100),
+        environment: Joi.string().valid(...projectConfig.environments),
+        context: Joi.object(),
+
+        // @TODO: one or all below
+        expectedToBeEnabled: Joi.boolean().required(),
+        expectedVariation: Joi.alternatives().try(Joi.string(), Joi.number(), Joi.boolean()),
+        expectedVariables: Joi.object(),
+      }),
+    ),
+  });
+
+  return Joi.alternatives().try(segmentTestJoiSchema, featureTestJoiSchema);
 }
 
 export function printJoiError(e: Joi.ValidationError) {
