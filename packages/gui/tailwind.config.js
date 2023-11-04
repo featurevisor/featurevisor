0434f36992a2950b8127eb7add1ff070eedaf70e
Fahad Heylaal <fahad19@gmail.com>
2023-11-05 00:13:22 +0100

    tailwind works!

--- packages/gui/tailwind.config.js
+++ packages/gui/tailwind.config.js
@@ -53,9 +53,9 @@ module.exports = {
         md: `calc(var(--radius) - 2px)`,
         sm: "calc(var(--radius) - 4px)",
       },
-      fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
-      },
+      // fontFamily: {
+      //   sans: ["var(--font-sans)", ...fontFamily.sans],
+      // },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
