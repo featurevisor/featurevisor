f2a57e5bcea8451d4199bb0ac28ac2a2dcdc137a
Fahad Heylaal <fahad19@gmail.com>
2023-11-19 23:55:45 +0100

    [skip ci] attribute overview with markdown content

--- packages/gui/src/components/routes/attribute-page-view.tsx
+++ packages/gui/src/components/routes/attribute-page-view.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { useParams } from "react-router-dom";
 
 import { Separator } from "../ui/separator";
+import { Markdown } from "../blocks/markdown";
 
 export function AttributePageView() {
   const { key } = useParams();
@@ -16,18 +17,53 @@ export function AttributePageView() {
   return (
     <div className="space-y-6">
       <div>
-        <h3 className="text-lg font-medium">Attribute: {key}</h3>
-        <p className="text-sm text-muted-foreground">Overview of your attribute</p>
+        <h3 className="text-lg font-medium">Overview</h3>
+        <p className="text-sm text-muted-foreground hidden">Overview of your attribute</p>
       </div>
 
       <Separator />
 
       {attribute ? (
-        <ul>
-          <li>Description: {attribute.description}</li>
-          <li>Type: {attribute.type}</li>
-          <li>Capture: {attribute.capture ? "Yes" : "No"}</li>
-        </ul>
+        <div className="border-gray-200">
+          <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+            <div>
+              <dt className="text-sm font-medium text-gray-500">Key</dt>
+              <dd className="mt-1 text-sm text-gray-900">
+                <code>{attribute.key}</code>
+              </dd>
+            </div>
+
+            <div>
+              <dt className="text-sm font-medium text-gray-500">Type</dt>
+              <dd className="mt-1 text-sm text-gray-900">{attribute.type}</dd>
+            </div>
+
+            <div>
+              <dt className="text-sm font-medium text-gray-500">Capture</dt>
+              <dd className="mt-1 text-sm text-gray-900">
+                {attribute.capture === true ? <span>Yes</span> : <span>No</span>}
+              </dd>
+            </div>
+
+            <div>
+              <dt className="text-sm font-medium text-gray-500">Archived</dt>
+              <dd className="mt-1 text-sm text-gray-900">
+                {attribute.archived === true ? <span>Yes</span> : <span>No</span>}
+              </dd>
+            </div>
+
+            <div className="col-span-2">
+              <dt className="text-sm font-medium text-gray-500">Description</dt>
+              <dd className="mt-1 text-sm text-gray-900">
+                {attribute.description.trim().length > 0 ? (
+                  <Markdown children={attribute.description} />
+                ) : (
+                  "n/a"
+                )}
+              </dd>
+            </div>
+          </dl>
+        </div>
       ) : (
         <p>Loading...</p>
       )}
