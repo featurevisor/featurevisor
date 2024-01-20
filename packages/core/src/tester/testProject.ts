e19f929c58a0fb6a95f08de34dc6fba94842345e
Fahad Heylaal <fahad19@gmail.com>
2024-01-20 01:24:01 +0100

    chore: make test runner DRY (#252)

--- packages/core/src/tester/testProject.ts
+++ packages/core/src/tester/testProject.ts
@@ -16,11 +16,82 @@ export interface TestProjectOptions {
   showDatafile?: boolean;
 }
 
+export interface TestPatterns {
+  keyPattern?: RegExp;
+  assertionPattern?: RegExp;
+}
+
+export interface ExecutionResult {
+  passed: boolean;
+  assertionsCount: {
+    passed: number;
+    failed: number;
+  };
+}
+
+export async function executeTest(
+  testFile: string,
+  deps: Dependencies,
+  options: TestProjectOptions,
+  patterns: TestPatterns,
+): Promise<ExecutionResult | undefined> {
+  const { datasource, projectConfig, rootDirectoryPath } = deps;
+
+  const testFilePath = datasource.getTestSpecName(testFile);
+
+  const t = await datasource.readTest(testFile);
+
+  const tAsSegment = t as TestSegment;
+  const tAsFeature = t as TestFeature;
+  const key = tAsSegment.segment || tAsFeature.feature;
+  const type = tAsSegment.segment ? "segment" : "feature";
+
+  const executionResult: ExecutionResult = {
+    passed: true,
+    assertionsCount: {
+      passed: 0,
+      failed: 0,
+    },
+  };
+
+  if (!key) {
+    console.error(`  => Invalid test: ${JSON.stringify(t)}`);
+    executionResult.passed = false;
+
+    return executionResult;
+  }
+
+  if (patterns.keyPattern && !patterns.keyPattern.test(key)) {
+    return;
+  }
+
+  let testResult;
+  if (type === "segment") {
+    testResult = await testSegment(datasource, tAsSegment, patterns);
+  } else {
+    testResult = await testFeature(datasource, projectConfig, tAsFeature, options, patterns);
+  }
+
+  printTestResult(testResult, testFilePath, rootDirectoryPath);
+
+  if (!testResult.passed) {
+    executionResult.passed = false;
+
+    executionResult.assertionsCount.failed = testResult.assertions.filter((a) => !a.passed).length;
+    executionResult.assertionsCount.passed +=
+      testResult.assertions.length - executionResult.assertionsCount.failed;
+  } else {
+    executionResult.assertionsCount.passed = testResult.assertions.length;
+  }
+
+  return executionResult;
+}
+
 export async function testProject(
   deps: Dependencies,
   options: TestProjectOptions = {},
 ): Promise<boolean> {
-  const { rootDirectoryPath, projectConfig, datasource } = deps;
+  const { projectConfig, datasource } = deps;
 
   let hasError = false;
 
@@ -42,7 +113,7 @@ export async function testProject(
 
   const startTime = Date.now();
 
-  const patterns = {
+  const patterns: TestPatterns = {
     keyPattern: options.keyPattern ? new RegExp(options.keyPattern) : undefined,
     assertionPattern: options.assertionPattern ? new RegExp(options.assertionPattern) : undefined,
   };
@@ -54,58 +125,21 @@ export async function testProject(
   let failedAssertionsCount = 0;
 
   for (const testFile of testFiles) {
-    const testFilePath = datasource.getTestSpecName(testFile);
+    const executionResult = await executeTest(testFile, deps, options, patterns);
 
-    const t = await datasource.readTest(testFile);
-
-    if ((t as TestSegment).segment) {
-      // segment testing
-      const test = t as TestSegment;
-
-      if (patterns.keyPattern && !patterns.keyPattern.test(test.segment)) {
-        continue;
-      }
-
-      const testResult = await testSegment(datasource, test, patterns);
-      printTestResult(testResult, testFilePath, rootDirectoryPath);
-
-      if (!testResult.passed) {
-        hasError = true;
-        failedTestsCount++;
-
-        failedAssertionsCount += testResult.assertions.filter((a) => !a.passed).length;
-        passedAssertionsCount += testResult.assertions.length - failedAssertionsCount;
-      } else {
-        passedTestsCount++;
-
-        passedAssertionsCount += testResult.assertions.length;
-      }
-    } else if ((t as TestFeature).feature) {
-      // feature testing
-      const test = t as TestFeature;
-
-      if (patterns.keyPattern && !patterns.keyPattern.test(test.feature)) {
-        continue;
-      }
-
-      const testResult = await testFeature(datasource, projectConfig, test, options, patterns);
-      printTestResult(testResult, testFilePath, rootDirectoryPath);
-
-      if (!testResult.passed) {
-        hasError = true;
-        failedTestsCount++;
-
-        failedAssertionsCount += testResult.assertions.filter((a) => !a.passed).length;
-        passedAssertionsCount += testResult.assertions.length - failedAssertionsCount;
-      } else {
-        passedTestsCount++;
-
-        passedAssertionsCount += testResult.assertions.length;
-      }
-    } else {
-      console.error(`  => Invalid test: ${JSON.stringify(test)}`);
-      hasError = true;
+    if (!executionResult) {
+      continue;
     }
+
+    if (executionResult.passed) {
+      passedTestsCount += 1;
+    } else {
+      hasError = true;
+      failedTestsCount += 1;
+    }
+
+    passedAssertionsCount += executionResult.assertionsCount.passed;
+    failedAssertionsCount += executionResult.assertionsCount.failed;
   }
 
   const diffInMs = Date.now() - startTime;
