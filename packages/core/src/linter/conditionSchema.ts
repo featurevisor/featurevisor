2dfff139c7c0234d088483fc5ed7321b1edf450d
Fahad Heylaal <fahad19@gmail.com>
2024-02-06 22:56:33 +0100

    [skip ci] conditions zod schema

--- packages/core/src/linter/conditionSchema.ts
+++ packages/core/src/linter/conditionSchema.ts
@@ -1,4 +1,5 @@
 import * as Joi from "joi";
+import { z } from "zod";
 
 import { ProjectConfig } from "../config";
 
@@ -111,4 +112,64 @@ export function getConditionsJoiSchema(
   );
 
   return conditionsJoiSchema;
+}
+
+export function getConditionsZodSchema(
+  projectConfig: ProjectConfig,
+  availableAttributeKeys: [string, ...string[]],
+) {
+  const plainConditionZodSchema = z.object({
+    attribute: z.enum(availableAttributeKeys),
+    operator: z.enum([
+      "equals",
+      "notEquals",
+
+      // numeric
+      "greaterThan",
+      "greaterThanOrEquals",
+      "lessThan",
+      "lessThanOrEquals",
+
+      // string
+      "contains",
+      "notContains",
+      "startsWith",
+      "endsWith",
+
+      // semver (string)
+      "semverEquals",
+      "semverNotEquals",
+      "semverGreaterThan",
+      "semverGreaterThanOrEquals",
+      "semverLessThan",
+      "semverLessThanOrEquals",
+
+      // date comparisons
+      "before",
+      "after",
+
+      // array of strings
+      "in",
+      "notIn",
+    ]),
+    value: z.union([z.string(), z.number(), z.boolean(), z.date(), z.null()]),
+  });
+
+  const andOrNotConditionZodSchema = z.union([
+    z.object({
+      and: z.array(z.lazy(() => andOrNotConditionZodSchema)),
+    }),
+    z.object({
+      or: z.array(z.lazy(() => andOrNotConditionZodSchema)),
+    }),
+    z.object({
+      not: z.array(z.lazy(() => andOrNotConditionZodSchema)),
+    }),
+  ]);
+
+  const conditionZodSchema = z.union([andOrNotConditionZodSchema, plainConditionZodSchema]);
+
+  const conditionsZodSchema = z.union([conditionZodSchema, z.array(conditionZodSchema)]);
+
+  return conditionsZodSchema;
 }
