403eac02c21cf716092fb4aa6ea447b6bd7df732
Fahad Heylaal <fahad19@gmail.com>
2024-02-11 21:33:39 +0100

    feat: Linter overhaul (#259)

--- packages/core/src/tester/cliFormat.ts
+++ packages/core/src/tester/cliFormat.ts
@@ -2,3 +2,4 @@ export const CLI_FORMAT_RED = "\x1b[31m%s\x1b[0m";
 export const CLI_FORMAT_GREEN = "\x1b[32m%s\x1b[0m";
 
 export const CLI_FORMAT_BOLD = "\x1b[1m%s\x1b[0m";
+export const CLI_FORMAT_UNDERLINE = "\x1b[4m%s\x1b[0m";
