19116d36313d581b33acaa19b8617bc7e3685481
Fahad Heylaal <fahad19@gmail.com>
2024-06-24 23:11:49 +0200

    use array length

--- packages/core/src/info/index.ts
+++ packages/core/src/info/index.ts
@@ -21,7 +21,7 @@ export async function showProjectInfo(deps: Dependencies) {
     const feature = await datasource.readFeature(featureKey);
 
     if (feature.variablesSchema) {
-      variablesCount += Object.keys(feature.variablesSchema).length;
+      variablesCount += feature.variablesSchema.length;
     }
   }
 
