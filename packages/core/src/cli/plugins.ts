f8f8e050a29f55c59973b04336d9b74c403b9d66
Fahad Heylaal <fahad19@gmail.com>
2024-07-31 21:10:23 +0200

    lint plugin

--- packages/core/src/cli/plugins.ts
+++ packages/core/src/cli/plugins.ts
@@ -1,29 +1,26 @@
 import type { Plugin } from "./cli";
 
 import { initPlugin } from "../init";
-
-// @TODO: add these plugins
-// - [x] init
-// - [ ] lint
-// - [ ] build
-// - [ ] restore
-// - [ ] site
-// - [ ] serve
-// - [ ] generate-code
-// - [ ] find-duplicate-segments
-// - [ ] find-usage
-// - [ ] benchmark
-// - [ ] config
-// - [ ] evaluate
-// - [ ] assess-distribution
-// - [ ] info
+import { lintPlugin } from "../linter";
 
 // that do not require an existing project
 export const nonProjectPlugins: Plugin[] = [initPlugin];
 
 // that require an existing Featurevisor project
 export const projectBasedPlugins: Plugin[] = [
-  // @TODO: implement core plugins
+  lintPlugin,
+  // buildPlugin,
+  // restorePlugin,
+  // sitePlugin,
+  // servePlugin,
+  // generateCodePlugin,
+  // findDuplicateSegmentsPlugin,
+  // findUsagePlugin,
+  // benchmarkPlugin,
+  // configPlugin,
+  // evaluatePlugin,
+  // assessDistributionPlugin,
+  // infoPlugin,
 ];
 
 export const commonPlugins: Plugin[] = [];
