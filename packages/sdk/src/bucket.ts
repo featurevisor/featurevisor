7bb97c34f73bc6bf73060fadbc36a7f0a074119f
Meir Roth <12494197+meirroth@users.noreply.github.com>
2024-08-16 07:36:48 +0000

    switch to ohash

--- packages/sdk/src/bucket.ts
+++ packages/sdk/src/bucket.ts
@@ -1,4 +1,4 @@
-import * as murmurhash from "murmurhash";
+import { murmurHash } from "ohash";
 
 const HASH_SEED = 1;
 const MAX_HASH_VALUE = Math.pow(2, 32);
@@ -6,7 +6,7 @@ const MAX_HASH_VALUE = Math.pow(2, 32);
 export const MAX_BUCKETED_NUMBER = 100000; // 100% * 1000 to include three decimal places in the same integer value
 
 export function getBucketedNumber(bucketKey: string): number {
-  const hashValue = murmurhash.v3(bucketKey, HASH_SEED);
+  const hashValue = murmurHash(bucketKey, HASH_SEED);
   const ratio = hashValue / MAX_HASH_VALUE;
 
   return Math.floor(ratio * MAX_BUCKETED_NUMBER);
