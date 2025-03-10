7aed80b0daaba3a3ad2a524714a82dd145eaba36
Fahad Heylaal <fahad19@gmail.com>
2025-03-10 23:33:57 +0100

    list features without filtering

--- packages/core/src/list/index.ts
+++ packages/core/src/list/index.ts
@@ -1,10 +1,52 @@
 import { Dependencies } from "../dependencies";
 import { Plugin } from "../cli";
+import { ParsedFeature } from "@featurevisor/types";
 
 export async function listProject(deps: Dependencies) {
-  const { rootDirectoryPath, projectConfig } = deps;
+  const { rootDirectoryPath, projectConfig, datasource, options } = deps;
 
-  console.log("Listing project...");
+  if (options.features) {
+    const allFeatureKeys = await datasource.listFeatures();
+
+    if (allFeatureKeys.length === 0) {
+      console.log("No features found.");
+      return;
+    }
+
+    const result: ParsedFeature[] = [];
+    for (const featureKey of allFeatureKeys) {
+      const feature = await datasource.readFeature(featureKey);
+
+      // @TODO: filter
+
+      result.push({
+        ...feature,
+        key: featureKey,
+      });
+    }
+
+    if (result.length === 0) {
+      console.log("No features found.");
+      return;
+    }
+
+    if (options.print) {
+      console.log(JSON.stringify(result));
+      return;
+    }
+
+    console.log("\nFeatures:\n");
+
+    for (const feature of result) {
+      console.log(`- ${feature.key}`);
+    }
+
+    console.log(`\n\nFound ${result.length} features.`);
+
+    return;
+  }
+
+  console.log("Nothing to list. Please pass `--features`, `--segments`, or `--attributes`.");
 }
 
 export const listPlugin: Plugin = {
