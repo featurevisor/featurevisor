cf3ea33b97edc921d2fae2ddf523c34b4d94b57d
Fahad Heylaal <fahad19@gmail.com>
2025-04-09 20:39:01 +0200

    remove old datafilereader tests

--- packages/sdk/src/datafileReader.spec.ts
+++ packages/sdk/src/datafileReader.spec.ts
@@ -6,87 +6,6 @@ describe("sdk: DatafileReader", function () {
     expect(typeof DatafileReader).toEqual("function");
   });
 
-  it("v1 datafile schema: should return requested entities", function () {
-    const datafileJson: DatafileContent = {
-      schemaVersion: "1",
-      revision: "1",
-      attributes: [
-        {
-          key: "userId",
-          type: "string",
-          capture: true,
-        },
-        {
-          key: "country",
-          type: "string",
-        },
-      ],
-      segments: [
-        {
-          key: "netherlands",
-          conditions: [
-            {
-              attribute: "country",
-              operator: "equals",
-              value: "nl",
-            },
-          ],
-        },
-        {
-          key: "germany",
-          conditions: JSON.stringify([
-            {
-              attribute: "country",
-              operator: "equals",
-              value: "de",
-            },
-          ]),
-        },
-      ],
-      features: [
-        {
-          key: "test",
-          bucketBy: "userId",
-          variations: [
-            { value: "control" },
-            {
-              value: "treatment",
-              variables: [
-                {
-                  key: "showSidebar",
-                  value: true,
-                },
-              ],
-            },
-          ],
-          traffic: [
-            {
-              key: "1",
-              segments: "*",
-              percentage: 100000,
-              allocation: [
-                { variation: "control", range: [0, 0] },
-                { variation: "treatment", range: [0, 100000] },
-              ],
-            },
-          ],
-        },
-      ],
-    };
-
-    const reader = new DatafileReader(datafileJson);
-
-    expect(reader.getRevision()).toEqual("1");
-    expect(reader.getSchemaVersion()).toEqual("1");
-    expect(reader.getAllAttributes()).toEqual(datafileJson.attributes);
-    expect(reader.getAttribute("userId")).toEqual(datafileJson.attributes[0]);
-    expect(reader.getSegment("netherlands")).toEqual(datafileJson.segments[0]);
-    expect((reader.getSegment("germany") as any).conditions[0].value).toEqual("de");
-    expect(reader.getSegment("belgium")).toEqual(undefined);
-    expect(reader.getFeature("test")).toEqual(datafileJson.features[0]);
-    expect(reader.getFeature("test2")).toEqual(undefined);
-  });
-
   it("v2 datafile schema: should return requested entities", function () {
     const datafileJson: DatafileContent = {
       schemaVersion: "2",
@@ -159,12 +78,6 @@ describe("sdk: DatafileReader", function () {
 
     expect(reader.getRevision()).toEqual("1");
     expect(reader.getSchemaVersion()).toEqual("2");
-    expect(reader.getAllAttributes()).toEqual(
-      Object.keys(datafileJson.attributes).reduce((acc, key) => {
-        acc.push(datafileJson.attributes[key]);
-        return acc;
-      }, [] as Attribute[]),
-    );
     expect(reader.getAttribute("userId")).toEqual(datafileJson.attributes.userId);
     expect(reader.getSegment("netherlands")).toEqual(datafileJson.segments.netherlands);
     expect((reader.getSegment("germany") as any).conditions[0].value).toEqual("de");
