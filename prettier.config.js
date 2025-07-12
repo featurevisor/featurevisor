fe4a7828c8bf179facbe75f0581776f182cdb0fd
Fahad Heylaal <fahad19@gmail.com>
2025-07-13 01:52:28 +0200

    prettier config for docs

--- prettier.config.js
+++ prettier.config.js
@@ -5,6 +5,19 @@ const config = {
   trailingComma: "all",
   tabWidth: 2,
   semi: true,
+
+  proseWrap: "preserve",
+  embeddedLanguageFormatting: "off",
+
+  overrides: [
+    {
+      files: ["*.md"],
+      options: {
+        proseWrap: "preserve",
+        embeddedLanguageFormatting: "off",
+      },
+    },
+  ],
 };
 
 module.exports = config;
