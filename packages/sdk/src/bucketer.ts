11bd056f4b461e62c0605cbfc3aa95c6618532d5
Fahad Heylaal <fahad19@gmail.com>
2025-07-20 23:44:40 +0200

    use bucket value type

--- packages/sdk/src/bucketer.ts
+++ packages/sdk/src/bucketer.ts
@@ -15,7 +15,7 @@ const MAX_HASH_VALUE = Math.pow(2, 32);
 
 export const MAX_BUCKETED_NUMBER = 100000; // 100% * 1000 to include three decimal places in the same integer value
 
-export function getBucketedNumber(bucketKey: string): number {
+export function getBucketedNumber(bucketKey: string): BucketValue {
   const hashValue = MurmurHashV3(bucketKey, HASH_SEED);
   const ratio = hashValue / MAX_HASH_VALUE;
 
