490168b1756918b3d8e915ed91303e2e155889c4
Fahad Heylaal <fahad19@gmail.com>
2025-12-07 20:33:51 +0100

    tests for scope

--- examples/example-yml-scopes/featurevisor.config.js
+++ examples/example-yml-scopes/featurevisor.config.js
@@ -8,6 +8,22 @@ module.exports = {
       tag: "web",
       context: { platform: "web" },
     },
+    {
+      name: "ios",
+      tag: "ios",
+      context: { platform: "ios" },
+    },
+    {
+      name: "android",
+      tag: "android",
+      context: { platform: "android" },
+    },
+    // @TODO: support multiple tags later
+    // {
+    //   name: "mobile",
+    //   tag: "mobile",
+    //   context: {},
+    // },
   ],
   prettyState: true,
   prettyDatafile: true, // enabled for readability while developing
