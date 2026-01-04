191936f431340e45697b45835b99a09ef6b220f2
Fahad Heylaal <fahad19@gmail.com>
2026-01-04 23:00:39 +0100

    todos

---
+++ packages/core/src/builder/buildScopedSegments.ts
@@ -0,0 +1,29 @@
+import type { GroupSegment, Context } from "@featurevisor/types";
+import type { DatafileReader } from "@featurevisor/sdk";
+
+export function buildScopedSegments(
+  datafileReader: DatafileReader,
+  segments: GroupSegment | GroupSegment[],
+  context: Context,
+): GroupSegment | GroupSegment[] {
+  const scoped = buildScopedGroupSegments(datafileReader, segments, context);
+  const removed = removeRedundantGroupSegments(scoped);
+
+  return removed;
+}
+
+export function removeRedundantGroupSegments(
+  groupSegments: GroupSegment | GroupSegment[],
+): GroupSegment | GroupSegment[] {
+  // @TODO: implement
+  return groupSegments;
+}
+
+export function buildScopedGroupSegments(
+  datafileReader: DatafileReader,
+  groupSegments: GroupSegment | GroupSegment[],
+  context: Context,
+): GroupSegment | GroupSegment[] {
+  // @TODO: implement
+  return groupSegments;
+}
